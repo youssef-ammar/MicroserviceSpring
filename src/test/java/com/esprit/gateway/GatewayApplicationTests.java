9a26d22185cc709ce1480f70a10badb8c7ee2b25
youssef-ammar <youssef.ammar19@yahoo.com>
2023-10-26 08:33:44 +0100

    gateway config

--- src/test/java/com/esprit/gateway/GatewayApplicationTests.java
+++ src/test/java/com/esprit/gateway/GatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.esprit.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class GatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
